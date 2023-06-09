68bed2c65209fad0e808b13ac0e2ad21b8bbbbe2
sssht03 <matsuo.shuta@arakawa-lab.com>
2023-06-10 03:32:08 +0900

    delete

--- src/components/SummaryToTitle.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h2>要約からタイトル生成</h2>
-</template>
-
-<script setup lang="ts">
-</script>
-
-
-<style scoped></style>
--- src/components/TextToSummary.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h2>テキストから要約生成</h2>
-</template>
-
-<script setup lang="ts">
-</script>
-
-
-<style scoped></style>
